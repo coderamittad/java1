738cf326eebfc562ec1e1f1cf5dd0750f97b6cba
coderamittad <amittad992004@gmail.com>
2026-03-27 18:59:18 +0530

    add code

--- a.java
+++ a.java
@@ -1,6 +1,10 @@
 public class a {
     public static void main(String[] args) {
         System.out.println("hellow world");
+        for (int i = 0; i < 10; i++) {
+              System.out.println(i);
+            
+        }
     }
     
 }
